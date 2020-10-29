6fa0f61963349140ff8c627f5953d0c3728e03c4
Gabriela Cabrera <gaby.cabrera9@gmail.com>
2020-10-29 15:35:38 -0400

    add step by step to enqueue and dequeue

--- queue.js
+++ queue.js
@@ -1,4 +1,23 @@
-//Application as linked list since creating a queue with arrays is inefficient (O(n)).
+//Implementation as linked list since creating a queue with arrays is inefficient (O(n)).
+
+//Linked List Review:
+
+    //A linked list consists of a series of nodes that point to each other. Each node is an object with 2 properties: value and next.
+
+    //A node's next property points to the next node in the list connecting all the node together sequentially.
+
+    //Because of this, linked lists are inefficient to read data. To find a node, you need to start by the first node and traverse the nodes until you reach thr right one.
+
+    //But since we dont need access to all nodes in a queue (only to the first one and the last one) we can disregard this downside.
+
+//To enqueue: we need to create a new node (which we will attach at the end of the list). This means we will make the next property of the last node currently in the list equal to the node we just created...
+//... then we will change the queue's tail pointer from the old tail to the new node since we need our queue object to reflect our new node as the new tail...
+//... finally, we will add one to the queue's length property, and take care of the edge case where this is the first node we add to the list. In this edge case, we need to update the queue's head property to equal our new node since at that point our unique node is both the tail and the head.
+
+//To dequeue: first, we will read the current head of the list so that we can return this node at the end...
+//... then we will change the head property of the queue to point to the second item in the list. We can refer to the second item by using the next property of the current head...
+//... then we will substract one to the queue's length property since we are removing one node from the list..
+//... finally we will return the old head of the queue, and take care of the edge case where we are removing the last node of the list. In this case we will need to change the tail property of the queue to null since there are no nodes remaining in the list.
 
 class Node {
     constructor(val) {
