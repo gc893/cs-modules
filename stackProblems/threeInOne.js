0958c7c1c48496610fdf3fdb8e883575ed8e577b
Gabriela Cabrera <gaby.cabrera9@gmail.com>
2020-10-29 16:59:29 -0400

    add ThreeInOne solution

--- stackProblems/threeInOne.js
+++ stackProblems/threeInOne.js
@@ -1,34 +1,75 @@
+//Use a single array to implement three stacks
+
 class Stack {
     constructor() {
         this.items = [];
-        
+        this.arrTwoHead = null;
+        this.arrThreeHead = null;
     }
 
-    push(element) {
-        this.items.push(element);
+    push(element, arr) {
+        if (this.items.length === 0) {
+            if (arr === 1) {
+                this.items.push(element)
+                this.arrTwoHead = 1;
+                this.arrThreeHead = 1;
+            } else if (arr === 2) {
+                this.items.push(element)
+                this.arrTwoHead = 0;
+                this.arrThreeHead = 1;
+            } else {
+                this.items.push(element);
+                this.arrTwoHead = 0;
+                this.arrThreeHead = 0;
+            }
+        } else {
+            if (arr === 1) {
+                this.items.splice(this.arrTwoHead,0,element)
+                this.arrTwoHead++;
+                this.arrThreeHead++;
+            } else if (arr === 2) {
+                this.items.splice(this.arrThreeHead,0,element)
+                this.arrThreeHead++;
+            } else {
+                this.items.push(element)
+            }
+        }
     }
 
-    pop() {
+    pop(arr) {
         if(this.items.length === 0) return undefined;
-        return this.items.pop();
-    }
 
-    peek() {
-        return this.items[this.items.length -1];
-    }
-
-    size() {
-        return this.items.length;
-    }
-
-    isEmpty() {
-        return !this.items.length;
+        if (arr === 1) {
+            let removed = this.items.splice(this.arrTwoHead -1,1);
+            this.arrTwoHead--;
+            this.arrThreeHead--;
+            return removed[0];
+        } else if (arr === 2) {
+            let removed = this.items.splice(this.arrThreeHead -1,1)
+            this.arrThreeHead--;
+            return removed[0];
+        } else {
+            return this.items.pop()
+        }
     }
 
     toString() {
-        return this.items.join('-');
-        // return this.items.reverse().join('->');
+        let arrays = [
+            this.items.slice(0, this.arrTwoHead), 
+            this.items.slice(this.arrTwoHead, this.arrThreeHead), 
+            this.items.slice(this.arrThreeHead, this.items.length)
+        ]
+        return arrays;
     }
 }
 
-// Use a single array to implement three stacks
+let threeInOne = new Stack();
+
+threeInOne.push(1,1);
+threeInOne.push(10,2)
+threeInOne.push(100,2)
+threeInOne.pop(1);
+threeInOne.pop(2);
+threeInOne.pop(3);
+
+console.log(threeInOne.toString());
